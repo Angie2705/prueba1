9fe51cc4e972a5d4f41dd29a8b69719bc92c9037
Angie2705 <ang.garcia@duocuc.cl>
2023-11-22 09:29:23 -0300

    icono

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'prueba1',
+  appName: 'ADGO',
   webDir: 'www',
   server: {
     androidScheme: 'https'
